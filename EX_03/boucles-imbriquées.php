9084a0404acb29b114f87e1035f8fa3820997c05
LorisFeist <67264659+LorisFeist@users.noreply.github.com>
2020-07-01 10:34:21 +0200

    Add files via upload

--- EX_03/boucles-imbriquées.php
+++ EX_03/boucles-imbriquées.php
@@ -1,26 +1,18 @@
 <?php
 
-$heures= 0;
-$minutes= 0;
-
-function clock($heure, $minutes)
-{
-    for($minutes=00; $minutes<=59; $minutes=$minutes + 1)
-    $minutes = $minutes + 1;
-    echo '00:'.$minutes.'<br>';
-{
-    if ($minutes > 59);
-    $heures = $heures +1; 
-    echo ''.$heures.':'.$minutes.'<br>';
-    
-    elseif (!$heures > 23 )
-    $heures = 0;  
-    echo "<p>00:00</p>";
-}
-}
+function clock()
+{   
+    for ($heures = 00; $heures < 24; $heures++)
+    {
+        for ($minutes = 00; $minutes < 60; $minutes++)
+            {
+                echo "$heures : $minutes<br>";
+            }
+        echo "<br>";
+    }
 return;
 }
+clock();
 
-clock((3600))
-    
 ?>
+    
